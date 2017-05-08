5234dc8bf22ab2101558682a701497a69cd9bfd7
Anwesh43 <anweshthecool0@gmail.com>
2017-05-08 14:49:20 +0530

    Using VertivcalColorFilterList in app

--- app/src/main/java/com/anwesome/ui/verticalcolorfilterlistdemo/MainActivity.java
+++ app/src/main/java/com/anwesome/ui/verticalcolorfilterlistdemo/MainActivity.java
@@ -1,7 +1,11 @@
 package com.anwesome.ui.verticalcolorfilterlistdemo;
 
+import android.graphics.BitmapFactory;
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.anwesome.ui.verticalcolorfilterlist.VerticalColorFilterList;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        VerticalColorFilterList verticalColorFilterList = new VerticalColorFilterList(this, BitmapFactory.decodeResource(getResources(),R.drawable.back3));
+        int colors[] = {Color.CYAN,Color.GREEN,Color.BLUE,Color.RED,Color.BLACK};
+        for(int color:colors) {
+            verticalColorFilterList.addColor(color);
+        }
+        verticalColorFilterList.show();
     }
 }
