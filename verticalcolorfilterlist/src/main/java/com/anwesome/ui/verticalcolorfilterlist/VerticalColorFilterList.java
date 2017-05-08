01dcd3fa3d65b092a0d36cc84f9ab9b231e3a232
Anwesh43 <anweshthecool0@gmail.com>
2017-05-08 10:21:16 +0530

    adding verticalcolorfilterlist

---
+++ verticalcolorfilterlist/src/main/java/com/anwesome/ui/verticalcolorfilterlist/VerticalColorFilterList.java
@@ -0,0 +1,25 @@
+package com.anwesome.ui.verticalcolorfilterlist;
+
+import android.app.Activity;
+import android.graphics.Bitmap;
+
+/**
+ * Created by anweshmishra on 08/05/17.
+ */
+public class VerticalColorFilterList {
+    private Activity activity;
+    private boolean isShown = false;
+    public VerticalColorFilterList(Activity activity, Bitmap bitmap) {
+        this.activity = activity;
+    }
+    public void addColor(int color) {
+        if(!isShown) {
+
+        }
+    }
+    public void show() {
+        if(!isShown) {
+            isShown = true;
+        }
+    }
+}
